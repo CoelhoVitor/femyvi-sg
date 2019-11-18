a0290926ebb65691624d7415671644d2928a4aaa
Felipeb61 <felipe.batistela61@gmail.com>
2019-11-17 22:21:31 -0300

    add users.xml

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,24 @@ import connection.Ports;
 import connection.UserAuth;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         HealthChecker hc = new HealthChecker(Ports.HEALTHCHECK);
         FileUpload fup = new FileUpload(Ports.UPLOAD);
         FileRemove fr = new FileRemove(Ports.REMOVE);
         UserAuth ua = new UserAuth(Ports.AUTH);
-        
+
         // Starting HealthCheck Thread on Port 3000
         hc.start();
-        
+
         // Start File Upload Thread on Port 3003
         fup.start();
-        
+
         // Start File Remove Thread on Port 3002
         fr.start();
-        
+
         // Start User Auth Thread on Port 3004
         ua.start();
     }
-    
+
 }
--- src/service/HashService.java
+++ src/service/HashService.java
@@ -7,30 +7,28 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.User;
 
-public class HashService {  
-    
+public class HashService {
+
     public boolean compareHash(User userDb, User userToAuth) {
         return generateHash(userToAuth.getPassword()).equals(userDb.getPassword());
     }
-    
+
     private String generateHash(String password) {
         try {
-            
+
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             byte messageDigest[] = md.digest(password.getBytes("UTF-8"));
-            
+
             StringBuilder hexPassword = new StringBuilder();
             for (byte b : messageDigest) {
                 hexPassword.append(String.format("%02X", 0xFF & b));
             }
-            
-            return hexPassword.toString();
-            
+            return hexPassword.toString().toLowerCase();
         } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
             Logger.getLogger(HashService.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return null;
-    }  
-    
+    }
+
 }
